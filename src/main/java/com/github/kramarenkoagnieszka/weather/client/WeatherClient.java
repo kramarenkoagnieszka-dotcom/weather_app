642d0a03503227c1790579a33954db683cc8c733
Agnieszka Kramarenko <akramarenko@griddynamics.com>
2026-05-02 09:29:59 +0200

    chore: delete old files

--- src/main/java/com/github/kramarenkoagnieszka/weather/client/WeatherClient.java
+++
@@ -1,9 +0,0 @@
-package com.github.kramarenkoagnieszka.weather.client;
-
-import com.github.kramarenkoagnieszka.weather.exception.WeatherClientException;
-import com.github.kramarenkoagnieszka.weather.model.City;
-
-public interface WeatherClient {
-
-  double getTemperature(City city) throws WeatherClientException;
-}
--- src/main/java/com/github/kramarenkoagnieszka/weather/exception/InvalidCityException.java
+++
@@ -1,7 +0,0 @@
-package com.github.kramarenkoagnieszka.weather.exception;
-
-public class InvalidCityException extends WeatherApplicationException {
-  public InvalidCityException(String message) {
-    super(message);
-  }
-}
